58a14b45c817f1b73d91d09afaadb11821cd773c
Adam Griepentrog <adamgriepentrog@Adams-iMac.local>
2018-10-23 14:55:24 -0500

    added schema and controller.js

---
+++ controllers/exercise-controller.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const router = express.Router();
+
+const db = require("../models");
+
+router.get("/" , function(req,res) {
+
+});
+
--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,38 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+CREATE DATABASE myWorkouts_DB;
+
+USE myWorkouts_DB;
+
+CREATE TABLE workouts (
+
+	id INT AUTO_INCREMENT PRIMARY KEY,
+
+    workout_name VARCHAR(255) NOT NULL,
+
+    date_logged DATE,
+
+    sets INT,
+
+    reps INT,
+
+    max_weight INT,
+
+    userId INT,
+
+    FOREIGN KEY (userId)
+
+		REFERENCES users(id)
+
+);
+
+CREATE TABLE users (
+
+	id INT AUTO_INCREMENT PRIMARY KEY,
+
+    username VARCHAR(255) NOT NULL,
+
+    current_weight FLOAT,
+
+    body_fat FLOAT
+
+);
