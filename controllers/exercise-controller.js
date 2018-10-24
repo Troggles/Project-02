ac4cca79cfd970597acf032eeb0a91c6e3f22586
Adam Griepentrog <adamgriepentrog@Adams-iMac.local>
2018-10-24 11:49:23 -0500

    controller update

--- controllers/exercise-controller.js
+++ controllers/exercise-controller.js
@@ -5,6 +5,27 @@ const router = express.Router();
 const db = require("../models");
 
 router.get("/" , function(req,res) {
+    res.redirect("/dashboard");
 
 });
+
+router.post("/dashboard/create", function(req, res) {
+    db.myWorkouts_DB.create({
+    workouts: req.body.workouts    
+    })
+
+    .then(function(myWorkouts_DB){
+        console.log(myWorkouts_DB);
+        res.redirect("/");
+    });
+});
+
+//do we want an update function?
+// router.put("/dashboard/update", function(req, res) {
+//     if(req.body.workouts) {
+
+//     }
+
+// })
+
 
