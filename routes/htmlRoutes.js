00441ec3300f491e43aa966382fb00f0ba36683f
Adam Griepentrog <adammiltron@gmail.com>
2018-10-25 19:43:28 -0500

    fixed htmlRoutes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,15 +5,13 @@ module.exports = app => {
 
   // Load home page
   app.get("/", (req, res) => {
-    db.myWorkouts_DB.findAll({}).then(WorkoutsDB => {
-      res.render("home", {
-        // Placeholder values, to be replaced with user inputs
-        user: "user placeholder",
-        lastBench: 200,
-        lastSquat: 305,
-        lastDead: 405,
-        b3Totals: 910
-      });
+    res.render("home", {
+      // Placeholder values, to be replaced with user inputs
+      user: "user placeholder",
+      lastBench: 200,
+      lastSquat: 305,
+      lastDead: 405,
+      b3Totals: 910
     });
   });
 
