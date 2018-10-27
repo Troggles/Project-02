d53c1a09803d6e087a6b8db5fe8b4df760bc849d
Ilya <ilyalutskiy@gmail.com>
2018-10-26 19:03:21 -0500

    added b3totals data

--- models/stats.js
+++ models/stats.js
@@ -1,8 +1,9 @@
 module.exports = (sequelize, DataTypes) => {
   const Stats = sequelize.define("Stats", {
-    date_stats: DataTypes.DATE,
+    date_stats: DataTypes.DATEONLY,
     new_weight: DataTypes.FLOAT,
     new_body_fat: DataTypes.FLOAT,
+    new_b3_total: DataTypes.INTEGER,
     userId: {
       type: DataTypes.INTEGER,
       allowNull: false
--- models/workout.js
+++ models/workout.js
@@ -1,19 +1,19 @@
 module.exports = (sequelize, DataTypes) => {
-    const Workout = sequelize.define("Workout", {
-        date_logged: DataTypes.DATE,
-        bench_sets: DataTypes.INTEGER,
-        bench_reps: DataTypes.INTEGER,
-        bench_max_weight: DataTypes.INTEGER,
-        squat_sets: DataTypes.INTEGER,
-        squat_reps: DataTypes.INTEGER,
-        squat_max_weight: DataTypes.INTEGER,
-        deadlift_sets: DataTypes.INTEGER,
-        deadlift_reps: DataTypes.INTEGER,
-        deadlift_max_weight: DataTypes.INTEGER,
-        userId: {
-            type: DataTypes.INTEGER,
-            allowNull: false
-        },
-    });
-    return Workout;
+  const Workout = sequelize.define("Workout", {
+    date_logged: DataTypes.DATE,
+    bench_sets: DataTypes.INTEGER,
+    bench_reps: DataTypes.INTEGER,
+    bench_max_weight: DataTypes.INTEGER,
+    squat_sets: DataTypes.INTEGER,
+    squat_reps: DataTypes.INTEGER,
+    squat_max_weight: DataTypes.INTEGER,
+    deadlift_sets: DataTypes.INTEGER,
+    deadlift_reps: DataTypes.INTEGER,
+    deadlift_max_weight: DataTypes.INTEGER,
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
+  });
+  return Workout;
 };
